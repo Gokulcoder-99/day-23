cf7e4c4ac998d3fa76e6c4b35c10f033d8633556
Gokulakrishnan <122555543+Gokulcoder-99@users.noreply.github.com>
2023-04-16 05:06:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,125 +1,87 @@
 import './App.css';
-import {useState} from "react";
-import productDetails from "./productDetails.json";
-
-
-// The product details are provided in "productDetails.json" file
-
 function App() {
-
-  // Setting initial state for the cart-count value
-  const [cartCount, setCartCount] = useState(0);
-
-  // Setting initial state for the array of ids (of the products) which are added to cart
-  const [idsInCart, setIdsInCart] = useState([]);
-
-
-  // The "addToCartFun" function handles adding a product to the cart
-  const addToCartFun = (idToAddIs) => {
-      setIdsInCart((idsInCart)=>idsInCart.concat([idToAddIs]));
-      setCartCount(cartCount+1);
-  }
-
-  // The "removeFromCartFun" function handles removing a product from the cart
-  const removeFromCartFun = (idToRemoveIs) => {
-      setIdsInCart((idsInCart)=>idsInCart.filter((idVal)=> idVal !== idToRemoveIs));
-      setCartCount(cartCount-1);
-  }
-    
-
-  // Returning data to be displayed on the web-page
     return(
-      // "main-div" class div holds the complete web-page
-        <div className="main-div">
-
-          {/* "navbar-div" class is for Navbar */}
-          <div className="navbar-div" >
-
-          {/* "navbar-sub-div-1" class div displays the "Home", "About", "Shop" */}
-            <div className="navbar-sub-div-1" >
-              <span>Home</span>
-              <span>About</span>
-              <span>Shop</span>
+<section className="pricing py-5">
+  <div className="container">
+    <div className="row">
+      <div className="col-lg-4">
+        <div className="card mb-5 mb-lg-0">
+          <div className="card-body">
+            <h5 className="card-title text-muted text-uppercase text-center">Free</h5>
+            <h6 className="card-price text-center">$0<span className="period">/month</span></h6>
+            <hr/>
+            <ul className="fa-ul">
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>Single User</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>5GB Storage</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Public Projects</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>Community Access</li>
+              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Unlimited
+                Private Projects</li>
+              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Dedicated
+                Phone Support</li>
+              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Free Subdomain
+              </li>
+              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Monthly Status
+                Reports</li>
+            </ul>
+            <div className="d-grid">
+              <a href="#" className="btn btn-primary text-uppercase">Button</a>
             </div>
-
-            {/* "navbar-sub-div-2" class div displays the cart-count */}
-            <div className="navbar-sub-div-2">
-
-              
-              <span>Cart</span>
-              <span> 
-                  {cartCount} 
-              </span>
-
-            </div>
-
-          </div>   
-
-
-          {/* "middle-headings-div" class div display the two-middle headings */}
-          <div className="middle-headings-div">
-              <h1 className="middle-heading-1" >Shop in style</h1>
-              <h5 className="middle-heading-2" >With this shop homepage template</h5>
           </div>
-
-
-          {/* "div-containing-all-products" class div displays all the products */}
-          <div className="div-containing-all-products" style={{display:'flex', flexDirection:"row", flexWrap:'wrap'}}>
-              
-              {/* Applying map function to iterate through the data of all the products */}
-              {productDetails.map(product=>{
-
-                // "each-product-div" class div displays the data of single product at a time
-                return <div className="each-product-div" key={product.id.toString()}>
-
-                  {/* Displaying the image of the product */}
-                  <img src={product.image} width="230px" height="150px" alt="missing"/>
-
-                  <br />
-
-                  {/* Displaying the category of the product */}
-                  <h5 className="product-category-h">{product.category}</h5>
-
-                  {/* Displaying the title or name of the product */}
-                  <p className="product-name-p">{product.title}</p>
-              
-                  {/* If rating is available for that product then displaying the rating on a scale of 5 */}
-                  {product.rating?
-                      (<p className='rating-p' >Rating: <b>{product.rating}</b><span>/5</span></p>)
-                  :
-                      (<p>Not yet rated</p>)}
-
-
-                    {/* Displaying the price of the product */}
-                  {(product.category==="Sale Item" || product.category==="Special Item")? 
-                      (<p className="sale-special-price-p" >
-                        <span>{"$"+product["old-price"]}</span> 
-                        <span>{"$"+product.price}</span>
-                      </p>)
-                  : product.category==="Popular Item" ? 
-                      (<p><span>{"$"+product.price}</span></p>)
-                  :
-                      (<p className="remining-price-p">
-                        <span className="product-price-span-1">{"$"+product["old-price"]}</span>
-                        <span>-</span> <span className="product-price-span-2">{"$"+product.price}</span>
-                      </p>)}
-
-                  
-                  {/* Checking whether the id of the product is in "idsInCart" array */}
-                  {/* If it is already added to the cart then showing "Remove from cart" and assigning "removeFromCartFun" function */}
-                  {/* If it is not added to the cart then showing "Add to cart" and assigning "addToCartFun" function */}
-                  {idsInCart.includes(product.id)? 
-                      (<button className="add-button" onClick={()=>removeFromCartFun(product.id)}>Remove from cart</button>)
-                  :
-                      (<button className="remove-button" onClick={()=>addToCartFun(product.id)}>Add to cart</button>)
-                  }
-
-                </div>
-              })}
-
-          </div>            
+        </div>
+      </div>
+      <div className="col-lg-4">
+        <div className="card mb-5 mb-lg-0">
+          <div className="card-body">
+            <h5 className="card-title text-muted text-uppercase text-center">Plus</h5>
+            <h6 className="card-price text-center">$9<span className="period">/month</span></h6>
+            <hr/>
+            <ul className="fa-ul">
+              <li><span className="fa-li"><i className="fas fa-check"></i></span><strong>5 Users</strong></li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>50GB Storage</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Public Projects</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>Community Access</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Private Projects</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>Dedicated Phone Support</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>Free Subdomain</li>
+              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Monthly Status
+                Reports</li>
+            </ul>
+            <div className="d-grid">
+              <a href="#" className="btn btn-primary text-uppercase">Button</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-lg-4">
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title text-muted text-uppercase text-center">Pro</h5>
+            <h6 className="card-price text-center">$49<span className="period">/month</span></h6>
+            <hr/>
+            <ul className="fa-ul">
+              <li><span className="fa-li"><i className="fas fa-check"></i></span><strong>Unlimited Users</strong>
+              </li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>150GB Storage</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Public Projects</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>Community Access</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Private Projects</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>Dedicated Phone Support</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span><strong>Unlimited</strong> Free
+                Subdomains</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>Monthly Status Reports</li>
+            </ul>
+            <div className="d-grid">
+              <a href="#" className="btn btn-primary text-uppercase">Button</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
+</section>
     )
 }
 
+export default App;
 export default App;
